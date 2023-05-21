983b0638a17eefab009fb06b4567c95fd66470eb
juchangyang <cksdid3357@naver.com>
2023-05-21 21:12:23 +0900

    수정
    수정

---
+++ checklist/lib/homePage.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class Group {
+  final String id;
+  final String name;
+  final String createdBy;
+
+  Group({required this.id, required this.name, required this.createdBy});
+}
+
+class _HomePage extends State<HomePage> {
+  List<Group> groups = []; //그룹 리스트
+  TextEditingController groupNameController = TextEditingController();
+  late String currentUserID; //현재 로그인한 사용자
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  void getUser() { //사용자 받아오기
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      setState(() {
+        currentUserID = user as String;
+      });
+    });
+  }
+
+  Future<void> addGroup() async { //그룹 추가
+    String groupName = groupNameController.text;
+    if (groupName.isNotEmpty) {
+      try {
+        CollectionReference groupsRef =
+            FirebaseFirestore.instance.collection('groups');
+        DocumentReference docRef = await groupsRef.add({
+          'name': groupName,
+          'createdBy': currentUserID,
+        });
+        String groupId = docRef.id;
+        setState(() {
+          groups.add(Group(id: groupId, name: groupName, createdBy: currentUserID) as Group);
+        });
+        groupNameController.clear(); // 텍스트 필드 초기화
+      } catch (e) {
+        print('Error adding group: $e');
+      }
+    }
+  }
+
+  //inviteGroup 마저 수정 예정
+    void inviteGroup(int index) { //그룹원을 초대하기 위해 다이얼로그를 표시하는 역할
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          String memberId = ''; // 초대할 그룹원을 저장하는 변수
+
+          return AlertDialog(
+            title: Text('그룹원 초대'),
+            content: TextField(
+              onChanged: (value) {
+                memberId = value; // 입력된 그룹원을 변수에 저장
+              },
+              decoration: InputDecoration(
+                hintText: '그룹원 아이디',
+              ),
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  // 초대 동작 처리
+                  String groupId = groups[index].id;
+                  inviteMember(groupId, memberId);
+
+                  Navigator.pop(context); // 다이얼로그 닫기
+                },
+                child: Text('초대'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // 다이얼로그 닫기
+                },
+                child: Text('취소'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+  void inviteMember(String groupId, String memberId) { //그룹원 초대하는 동작 수행
+    try {
+      CollectionReference groupsRef =
+      FirebaseFirestore.instance.collection('groups');
+      // 초대할 그룹원의 정보를 업데이트하는 작업 수행
+      groupsRef.doc(groupId).update({
+        'invitedMembers': FieldValue.arrayUnion([memberId]),
+      });
+      print('그룹원을 성공적으로 초대했습니다.');
+    } catch (e) {
+      print('그룹원 초대 동작에 실패했습니다: $e');
+    }
+  }
+
+  Future<void> deleteGroup(String groupId) async { //그룹 삭제
+    try {
+      CollectionReference groupsRef =
+          FirebaseFirestore.instance.collection('groups');
+      await groupsRef.doc(groupId).delete();
+      setState(() {
+        groups.removeWhere((group) => group == groupId);
+      });
+    } catch (e) {
+      print('Error deleting group: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder( //그룹 추가 ui
+        itemCount: groups.length + 1,
+        itemBuilder: (context, index) {
+          if (index == groups.length) {
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Center(
+                  child: Text('그룹을 추가하세요',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20)),
+                ),
+              ),
+            );
+          } else {
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child:
+                          Text(groups[index] as String, style: TextStyle(fontSize: 20)),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        inviteGroup(index);
+                      },
+                      icon: Icon(Icons.edit),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        deleteGroup(groups[index] as String);
+                      },
+                      icon: Icon(Icons.delete),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton( //그룹 추가 버튼
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('그룹 생성'),
+                content: TextField(
+                  controller: groupNameController,
+                  decoration: InputDecoration(
+                    hintText: '그룹 이름',
+                  ),
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context); // 다이얼로그 닫기
+                      addGroup(); // 그룹 추가 함수 호출
+                    },
+                    child: Text('추가'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context); // 다이얼로그 닫기
+                    },
+                    child: Text('취소'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
