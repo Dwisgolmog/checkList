ca208b17cc9636868429c52508e12195e961c2d6
hhyukk <hhyukk1273@gmail.com>
2023-05-25 13:07:17 +0900

    전체적인 코드 수정
    코드 정리, 달력에서 주말 색 변경, firebase에서의 리스트 저장 방식 변

--- checklist/lib/main.dart
+++ checklist/lib/main.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:checklist/loginscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() async{
 
   //파이어베이스를 사용하기 위해 메인메소드 내에서 비동기 방식으로 아래의 메서드를 불러와야함
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(MyApp());
 }
 
