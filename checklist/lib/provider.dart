ad0adc26c0fc6fd38a70320205a3cf2d75ba4406
juchangyang <cksdid3357@naver.com>
2023-05-30 14:54:27 +0900

    Revert "재촉하기 버튼 구현"
    This reverts commit c3330599f0b42e6a476196087ef629813de4bad2.

--- checklist/lib/provider.dart
+++ checklist/lib/provider.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/foundation.dart';
 
 class VariableProvider with ChangeNotifier {
   String? groupName;
-  String? _selectedGroupName;
-  String get selectedGroupName => _selectedGroupName!;
 
   void setGroupName(String groupName) {
     this.groupName = groupName;
-    _selectedGroupName = groupName;
     notifyListeners();
-  }
-  bool _isPressed = false;
-  bool get isPressed => _isPressed;
-
-  void setWarning(bool value) {
-    _isPressed = value;
-    notifyListeners();
-  }
-  void setWarningForGroup(String groupName, bool isWarning) {
-    if (_selectedGroupName == groupName) {
-      _isPressed = isWarning;
-      notifyListeners();
-    }
   }
 }
